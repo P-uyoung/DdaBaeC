087c782cf9083586aeb8d316e2f97d6a6afcc4f7
P-uyoung <wooyoung9654@gmail.com>
2020-09-15 16:00:21 +0900

    initial commit

---
+++ prime_number.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+	unsigned num;
+	int isPrime=1;
+
+	scanf("%u", &num);
+
+
+	for (unsigned div = 2; div*div <= num; div++)
+	{
+		if (num % div == 0)
+		{
+			isPrime = 0;
+
+			if (num == div * div)
+				printf("%u is divisible by %u.\n", num, div);
+
+			else
+				printf("%u is divisible by %u and %u. \n", num, div, num / div);
+
+		}
+	}
+
+	if (isPrime)
+		printf("%u is a prime number.\n", num);
+			
+	
+
+	return 0;
+}
